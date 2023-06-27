ddeab9f71ab709dafc605758962c7e2c03732164
UserMadoka <u_umado@yahoo.co.jp>
2023-06-27 23:46:36 +0900

    Formオブジェクトを用いた購入情報の登録機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,26 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new
   end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+        @order_shipping.save
+        redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipping).permit(:postal_code, :city, :addresses, :building, :phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id], price: @item.price)
+  end
+
 end
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -2,13 +2,23 @@ class OrderShipping
   include ActiveModel::Model
   attr_accessor :price, :user_id, :item_id, :postal_code, :city, :addresses, :building, :phone_number, :prefecture_id, :order_id
 
-  validates :price,      presence: true
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :postal_code, presence: true
-  validates :city, presence: true
-  validates :addresses, presence: true
-  validates :phone_number, presence: true
-  validates :prefecture_id, presence: true
-  validates :order_id, presence: true
+  # 必須のチェック
+  with_options presence: true do
+      validates :user_id
+      validates :item_id
+      validates :city
+      validates :addresses
+  end
+
+  # 都道府県
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  # 郵便番号
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  # 電話番号
+  validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. 数字だけで入力してください"}
+
+  def save
+    order = Order.create(price: price, user_id: user_id, item_id: item_id)
+    Shipping.create(postal_code: postal_code, city: city, addresses: addresses, building: building, phone_number: phone_number, prefecture_id: prefecture_id, user_id: user_id, order_id: order.id)
+  end
 end
