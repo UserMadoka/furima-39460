06ce0070b10e2582a97c71be6427492e4241ee7b
UserMadoka <u_umado@yahoo.co.jp>
2023-06-28 15:53:42 +0900

    商品購入ページの条件分岐追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,14 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :select_item
+  before_action :redirect_to_myitem
+  before_action :redirect_to_soldout
 
   def index
-    @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new(order_params)
     if @order_shipping.valid?
         @order_shipping.save
@@ -21,6 +22,22 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_shipping).permit(:postal_code, :city, :addresses, :building, :phone_number, :prefecture_id).merge(user_id: current_user.id, item_id: params[:item_id], price: @item.price)
+  end
+
+  def select_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_to_soldout
+    if current_user.id != @item.user.id && Order.exists?(item_id: @item.id) 
+      return redirect_to root_path
+    end
+  end
+
+  def redirect_to_myitem
+    if current_user.id == @item.user.id
+      return redirect_to root_path
+    end
   end
 
 end
