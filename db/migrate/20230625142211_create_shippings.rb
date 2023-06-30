61ef7e210fe825bd513d47d6456ea5a3863cc088
UserMadoka <u_umado@yahoo.co.jp>
2023-06-30 17:03:43 +0900

    マイグレーションファイルの修正_01

--- db/migrate/20230625142211_create_shippings.rb
+++ db/migrate/20230625142211_create_shippings.rb
@@ -4,10 +4,9 @@ class CreateShippings < ActiveRecord::Migration[7.0]
       t.string     :postal_code,   null: false
       t.string     :city,          null: false
       t.string     :addresses,     null: false
-      t.string     :building,      null: true
+      t.string     :building
       t.string     :phone_number,  null: false
       t.integer    :prefecture_id, null: false
-      t.references :user,          null: false, foreign_key: true
       t.references :order,         null: false, foreign_key: true
       t.timestamps
     end
