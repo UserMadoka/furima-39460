c733068c8ddbe4c0aecd153267fdbd807765d084
UserMadoka <u_umado@yahoo.co.jp>
2023-06-30 21:10:13 +0900

    テストコミット分の修正

--- spec/models/order_shipping_spec.rb
+++ spec/models/order_shipping_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe OrderShipping, type: :model do
     @order_shipping = FactoryBot.build(:order_shipping, user_id: @user.id, item_id: @item.id)
   end
 
-
   describe '商品購入機能' do
     # 商品購入機能についてのテストコードを記述します
     context '購入ができる時' do
